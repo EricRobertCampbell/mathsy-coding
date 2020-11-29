a8248175e195692cc7f7fe415601f3eaecc0bab8
Eric Campbell <eric.robert.campbell@gmail.com>
2020-11-29 13:11:53 -0700

    Added icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
 				background_color: `#ffffff`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
+				icon: "src/images/icon.jpg",
 			},
 		},
 		`gatsby-plugin-react-helmet`,
