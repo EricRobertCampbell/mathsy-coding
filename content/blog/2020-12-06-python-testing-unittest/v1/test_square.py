2df9ea4699a03b0aad75428c6f836f504c09b342
Eric Campbell <eric.robert.campbell@gmail.com>
2020-12-13 21:34:00 -0700

    Finished Python testing with `unittest`
    Finished writing and revising 'Python Testing with `unittest`',
    including adding code samples.

--- content/blog/2020-12-06-python-testing-unittest/v1/test_square.py
+++
@@ -1,9 +0,0 @@
-# test_square.py
-import unittest
-from square import square
-
-
-class TestSquare(unittest.TestCase):
-    def test_correct_values(self):
-        """ When applied to numbers, the result should be correct """
-        self.assertEqual(square(1), 1)
