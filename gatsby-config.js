0a415d33d19b2e640d06fc02ad2ee8561bf91c11
Eric Campbell <eric.robert.campbell@gmail.com>
2021-12-08 11:17:37 -0700

    test: adding in gatsby-remark-mathjax

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
 					`gatsby-remark-prismjs`,
 					`gatsby-remark-copy-linked-files`,
 					`gatsby-remark-smartypants`,
-					// `gatsby-remark-mathjax`,
+					`gatsby-remark-mathjax`,
 				],
 			},
 		},
